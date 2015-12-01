6072a16bce91157ca2e8de0ff33871f90350d607
Sean Esteva <seangeleno@gmail.com>
2015-12-01 11:13:27 -0800

    added 2nd and 3rd controllers

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
   (function() {
     'use strict';
+    //john papa style guide
     angular.module('myApp',[])//the second parameter is because it needs to be a setter and not a getter?
     // $('#two').val() //here it is acting as a getter
     // $('#two').val('2') //here it is acting as a setter
+    //john papa way
+    angular.module('myApp')
+    .controller('myController',myController)
+    .controller('secondController',secondController)
+    .controller('thirdController',thirdController)
+
+    //myController function
+    function myController(){
+      var myCtrl = this
+      myCtrl.name = "The John Papa Way!"
+
+      myCtrl.setName = function(name){
+        myCtrl.name = name
+      }
+    }
+    function secondController(){
+      var sCtrl = this
+      sCtrl.name = "second controller"
+    }
+
+    function thirdController(){
+      var tCtrl = this
+      tCtrl.name = "BOOM I GOT DA CONTROLLER!"
+    }
 
   }());
 //use strict doesn't allow you to pollute the global variable space, will not let you override variables
---
+++ basic.js
@@ -0,0 +1,8 @@
+//this is the regular way
+    var app = angular.module('myApp',[])
+    //reg way
+    // app.controller('myController', function($scope){
+    //   $scope.name = 'My App'
+    
+    }
+  }());
