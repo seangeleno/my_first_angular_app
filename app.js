516d8d084a2af5e53b1c1cea58b005be909eee0b
Sean Esteva <seangeleno@gmail.com>
2015-12-01 10:32:45 -0800

    added custom html attribute

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
   (function() {
     'use strict';
+    angular.module('myApp',[])//the second parameter is because it needs to be a setter and not a getter?
+    // $('#two').val() //here it is acting as a getter
+    // $('#two').val('2') //here it is acting as a setter
 
   }());
 //use strict doesn't allow you to pollute the global variable space, will not let you override variables
-//if they have the same name. 
+//if they have the same name.
