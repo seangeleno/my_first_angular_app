a5ec811ec2b759490fcfab7b19e27859807b963f
Sean Esteva <seangeleno@gmail.com>
2015-12-01 10:26:27 -0800

    added use strict to the app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+  (function() {
+    'use strict';
+
+  }());
+//use strict doesn't allow you to pollute the global variable space, will not let you override variables
+//if they have the same name. 
